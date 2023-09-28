21b83e4ea1cbfbcc21ceb5b1dfd9087a2c431ce4
wailingling <145647673+wailingling@users.noreply.github.com>
2023-09-28 15:44:26 +0800

    28 Sep 2023_2nd Commit

--- script.js
+++ script.js
@@ -18,3 +18,75 @@ thumbnails.forEach(function(thumbnail) {
  });
 });
 
+/*
+// Photo Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlide");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
+*/
+
+//Auto Photo Slider
+// Set slide index to 1, this represents the current slide
+let slideIndex = 1;
+
+// Display the first slide
+showSlides(slideIndex);
+
+// Automatically move to the next slide every 2 seconds
+setInterval(function() {
+plusSlides(1);
+}, 3500); // Change to the desired number of milliseconds
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlide");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+
+slides[slideIndex-1].style.display = "block";
+if (dots.length > 0) { // Add this check in case there are no dots
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+}
+dots[slideIndex-1].className += " active";
+}
+}
+
+
